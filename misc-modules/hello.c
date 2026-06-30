20f87e52a38743f7e647572b5c3826603bb9fe1c
huuquy <huuquy99@gmail.com>
2026-06-30 21:43:56 +0700

    huuquy assignment 7: add github username to misc-modules/hello.c

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world from huuquy99\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world from huuquy99\n");
 }
 
 module_init(hello_init);
